a2ab5384a19410bc804c2abad7f5b9c6c2ea9cfa
igotyb <igotyb.stream@gmail.com>
2023-03-15 11:12:38 +0100

    comment console.log(data)

--- controller/addData.mjs
+++ controller/addData.mjs
@@ -5,15 +5,15 @@ import skillkedin_model from "./createTables.mjs";
 import * as fs from 'fs';
 
 const create_questions = async (req, res) => {
-    try{
+    try {
         const data = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'))
-        console.log(data)
+        // console.log(data)
         await skillkedin_model.create(data)
-        
+
         // const saved_questions = await question.save();
-         res.status(201).send('questions saved in the DB');
-    }catch(err){
-         console.log(err);
+        res.status(201).send('questions saved in the DB');
+    } catch (err) {
+        console.log(err);
     }
 };
 
--- controller/data.mjs
+++ controller/data.mjs
@@ -4,6 +4,6 @@ import * as fs from 'fs';
 // const fs = require('fs')
 
 const data = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'))
-console.log(data)
+// console.log(data)
 
 export default data; 
